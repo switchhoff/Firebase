e1ac8edca619a345ca70cf736cbd7ab1c13f2b97
Alex Hofmann <alex.hofmann@fortifyedge.com>
2026-02-16 11:29:50 +1100

    refactor: secure firebase credentials with env vars

--- apps/bintherestorethat/src/lib/firebase.ts
+++ apps/bintherestorethat/src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDs1Us2wlPibcCprKVaCnuFdAAUhJxEfY",
-  authDomain: "studio-5962175046-a9cb5.firebaseapp.com",
-  projectId: "studio-5962175046-a9cb5",
-  storageBucket: "studio-5962175046-a9cb5.appspot.com",
-  messagingSenderId: "1036746542561",
-  appId: "1:1036746542561:web:7efacf1bc0011271dad6a0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 
